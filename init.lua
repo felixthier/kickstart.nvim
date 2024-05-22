33ba56c4c534989ee25f02eac4dd223a30961c0c
Felix Thierfelder <felixthier@github.com>
2024-05-22 15:27:44 +0200

    fix typo

--- init.lua
+++ init.lua
@@ -90,7 +90,7 @@ P.S. You can delete this when you're done too. It's your config now! :)
 vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
 
-vim.opt.conceallevle = 2
+vim.opt.conceallevel = 2
 -- Set to true if you have a Nerd Font installed
 vim.g.have_nerd_font = true
 -- [[ Setting options ]]
