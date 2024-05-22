f921e30736a27e60c8ced4ceda26edfc54c97e92
Felix Thierfelder <felixthier@github.com>
2024-05-22 08:04:29 +0200

    add obsidian to config

--- init.lua
+++ init.lua
@@ -399,6 +399,37 @@ require('lazy').setup({
           require('copilot_cmp').setup()
         end,
       },
+      {
+        'epwalsh/obsidian.nvim',
+        version = '*', -- recommended, use latest release instead of latest commit
+        lazy = true,
+        event = {
+          -- If you want to use the home shortcut '~' here you need to call 'vim.fn.expand'.
+          -- E.g. "BufReadPre " .. vim.fn.expand "~" .. "/my-vault/**.md"
+          'BufReadPre path/to/my-vault/**.md',
+          'BufNewFile path/to/my-vault/**.md',
+        },
+        dependencies = {
+          -- Required.
+          'nvim-lua/plenary.nvim',
+
+          -- see below for full list of optional dependencies 👇
+        },
+        opts = {
+          workspaces = {
+            {
+              name = 'personal',
+              path = '~/vaults/personal',
+            },
+            -- {
+            --   name = 'work',
+            --   path = '~/vaults/work',
+            -- },
+          },
+
+          -- see below for full list of options 👇
+        },
+      },
     },
     config = function()
       -- Telescope is a fuzzy finder that comes with a lot of different things that
