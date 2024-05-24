1156d6b4396f6b7a3256b5402fb4104d6d546cc2
Felix Thierfelder <felixthier@github.com>
2024-05-24 15:15:48 +0200

    properly config obsidian, new ID creation

--- init.lua
+++ init.lua
@@ -400,38 +400,8 @@ require('lazy').setup({
           require('copilot_cmp').setup()
         end,
       },
-      {
-        'epwalsh/obsidian.nvim',
-        version = '*', -- recommended, use latest release instead of latest commit
-        lazy = true,
-        event = {
-          -- If you want to use the home shortcut '~' here you need to call 'vim.fn.expand'.
-          -- E.g. "BufReadPre " .. vim.fn.expand "~" .. "/my-vault/**.md"
-          'BufReadPre path/to/my-vault/**.md',
-          'BufNewFile path/to/my-vault/**.md',
-        },
-        dependencies = {
-          -- Required.
-          'nvim-lua/plenary.nvim',
-
-          -- see below for full list of optional dependencies 👇
-        },
-        opts = {
-          workspaces = {
-            {
-              name = 'personal',
-              path = '~/vaults/personal',
-            },
-            -- {
-            --   name = 'work',
-            --   path = '~/vaults/work',
-            -- },
-          },
-
-          -- see below for full list of options 👇
-        },
-      },
     },
+
     config = function()
       -- Telescope is a fuzzy finder that comes with a lot of different things that
       -- it can fuzzy find! It's more than just a "file finder", it can search
@@ -990,6 +960,64 @@ require('lazy').setup({
       --    - Show your current context: https://github.com/nvim-treesitter/nvim-treesitter-context
       --    - Treesitter + textobjects: https://github.com/nvim-treesitter/nvim-treesitter-textobjects
     end,
+  },
+  {
+    'epwalsh/obsidian.nvim',
+    version = '*', -- recommended, use latest release instead of latest commit
+    lazy = true,
+    event = {
+      -- If you want to use the home shortcut '~' here you need to call 'vim.fn.expand'.
+      -- E.g. "BufReadPre " .. vim.fn.expand "~" .. "/my-vault/**.md"
+      'BufReadPre '
+        .. vim.fn.expand '~'
+        .. '/vaults/personal/**.md',
+      'BufNewFile ' .. vim.fn.expand '~' .. '/vaults/personal/**.md',
+    },
+    dependencies = {
+      -- Required.
+      'nvim-lua/plenary.nvim',
+
+      -- see below for full list of optional dependencies 👇
+    },
+    opts = {
+      workspaces = {
+        {
+          name = 'personal',
+          path = '~/vaults/personal',
+        },
+        -- {
+        --   name = 'work',
+        --   path = '~/vaults/work',
+        -- },
+      },
+      -- Optional, customize how note IDs are generated given an optional title.
+      ---@param title string|?
+      ---@return string
+      note_id_func = function(title)
+        -- Create note IDs in a Zettelkasten format with a timestamp and a suffix.
+        -- In this case a note with the title 'My new note' will be given an ID that looks
+        -- like '1657296016-my-new-note', and therefore the file name '1657296016-my-new-note.md'
+        local suffix = ''
+        if title ~= nil then
+          -- If title is given, transform it into valid file name.
+          suffix = title:gsub(' ', '-'):gsub('[^A-Za-z0-9-]', ''):lower()
+        else
+          -- If title is nil, just add 4 random uppercase letters to the suffix.
+          for _ = 1, 4 do
+            suffix = suffix .. string.char(math.random(65, 90))
+          end
+        end
+        return tostring(os.date '%Y%m%d') .. '-' .. suffix
+      end,
+      -- Optional, customize how note file names are generated given the ID, target directory, and title.
+      ---@param spec { id: string, dir: obsidian.Path, title: string|? }
+      ---@return string|obsidian.Path The full path to the new note.
+      note_path_func = function(spec)
+        -- This is equivalent to the default behavior.
+        local path = spec.dir / tostring(spec.id)
+        return path:with_suffix '.md'
+      end,
+    },
   },
 
   -- The following two comments only work if you have downloaded the kickstart repo, not just copy pasted the
